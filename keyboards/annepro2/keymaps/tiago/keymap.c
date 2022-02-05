38e69655cf8839eda5d9a6e79ad5c52ee23e846f
Tiago Donato <tgdnt@pm.me>
2022-02-05 13:05:34 -0600

    Narrow to my keymap

---
+++ keyboards/annepro2/keymaps/tiago/keymap.c
@@ -0,0 +1,128 @@
+#include <stdint.h>
+#include "annepro2.h"
+#include "qmk_ap2_led.h"
+
+enum anne_pro_layers {
+  _BASE_LAYER,
+  _VIM_LAYER,
+  _FN_LAYER,
+};
+
+// This is ROW*MATRIX_COLS + COL
+#define CAPS_LOCATION (MATRIX_COLS * 2 + 0)
+
+const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  /*
+  * Layer _BASE_LAYER
+  * ,-----------------------------------------------------------------------------------------.
+  * | esc |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |    Bksp   |
+  * |-----------------------------------------------------------------------------------------+
+  * | Tab    |  q  |  w  |  e  |  r  |  t  |  y  |  u  |  i  |  o  |  p  |  [  |  ]  |   ~    |
+  * |-----------------------------------------------------------------------------------------+
+  * | Ctrl     |  a  |  s  |  d  |  f  |  g  |  h  |  j  |  k  |  l  |  ;  |  '  |    Enter   |
+  * |-----------------------------------------------------------------------------------------+
+  * | Shift      |  z  |  x  |  c  |  v  |  b  |  n  |  m  |  ,  |  .  |  /  |    Shift       |
+  * |-----------------------------------------------------------------------------------------+
+  * | AltGr  |  GUI   | Alt   |               space             | Left  | Down |  Up  | Right  |
+  * \-----------------------------------------------------------------------------------------/
+  * Layer HOLD in _BASE_LAYER
+  * ,-----------------------------------------------------------------------------------------.
+  * | FN  |     |     |     |     |     |     |     |     |     |     |     |     |           |
+  * |-----------------------------------------------------------------------------------------+
+  * | VIM    |     |     |     |     |     |     |     |     |     |     |     |     |        |
+  * |-----------------------------------------------------------------------------------------+
+  * |         |     |     |     |     |     |     |     |     |     |     |     |             |
+  * |-----------------------------------------------------------------------------------------+
+  * |            |     |     |     |     |     |     |     |     |     |     |                |
+  * |-----------------------------------------------------------------------------------------+
+  * |       |       |       |                                 |       |       |       |       |
+  * \-----------------------------------------------------------------------------------------/
+  */
+  [_BASE_LAYER] = KEYMAP( /* Base */
+    LT(_FN_LAYER, KC_ESC), KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL,   KC_BSPC,
+    LT(_VIM_LAYER, KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC,  KC_GRAVE,
+    KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,         KC_ENT,
+    KC_LSFT,KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+    KC_RALT,KC_LGUI, KC_LALT,                KC_SPC,               KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT
+  ),
+  /* Layer _VIM_LAYER
+  * ,-----------------------------------------------------------------------------------------.
+  * |     |     |     |     | End |     |     |     |     |    | Home |     |     |   DELETE  |
+  * |-----------------------------------------------------------------------------------------+
+  * |        |     |     |     |     |     |    | PGUP |     |     |     | Home| End |   \    |
+  * |-----------------------------------------------------------------------------------------+
+  * |         |     |    | PGDN |     |     | <-  | \/  |  ^  | ->  |      |      |           |
+  * |-----------------------------------------------------------------------------------------+
+  * |            |     |     |     |     |     |     |     |     |     |     |                |
+  * |-----------------------------------------------------------------------------------------+
+  * |       |       |  GUI  |                                 |       |       |       |       |
+  * \-----------------------------------------------------------------------------------------/
+  */
+  [_VIM_LAYER] = KEYMAP( /* Base */
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_END, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_TRNS, KC_TRNS,   KC_DELETE,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_END,  KC_BSLS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_PGDN, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_TRNS, KC_TRNS,         KC_TRNS,
+    KC_TRNS,KC_TRNS, KC_DELETE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS,KC_TRNS, KC_TRNS,      KC_TRNS,             KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS
+  ),
+  /* Layer _FN_LAYER
+  * ,-----------------------------------------------------------------------------------------.
+  * |     | F1  | F2  | F3  | F4  | F5  | F6  | F7  | F8  | F9  | F10 | F11 | F12 |           |
+  * |-----------------------------------------------------------------------------------------+
+  * |   USB  | BT1 | BT2 | BT3 | BT4 |     |     |     |     |     |     |     |     | LED_On |
+  * |-----------------------------------------------------------------------------------------+
+  * | BT_UN   |     |     |     |     |     | <-  | \/  |  ^  |  -> |     |     | LED_Off     |
+  * |-----------------------------------------------------------------------------------------+
+  * |            |     |     |     |     | M1  | M2  |     |     |     |     |                |
+  * |-----------------------------------------------------------------------------------------+
+  * |       |       |       |                                 |       |       |       |       |
+  * \-----------------------------------------------------------------------------------------/
+  */
+  [_FN_LAYER] = KEYMAP( /* Base */
+    KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS,
+    KC_AP2_USB, KC_AP2_BT1, KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, KC_TRNS, KC_TRNS, KC_MS_WH_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_WH_LEFT, KC_MS_WH_RIGHT, KC_AP_LED_ON,
+    KC_AP2_BT_UNPAIR, KC_TRNS, KC_TRNS, KC_MS_WH_DOWN, KC_TRNS, KC_TRNS, KC_MS_LEFT, KC_MS_DOWN, KC_MS_UP, KC_MS_RIGHT, KC_TRNS, KC_TRNS, KC_AP_LED_OFF,
+    KC_TRNS,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_BTN1, KC_MS_BTN2, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS,KC_TRNS, KC_TRNS,      KC_TRNS,             KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS
+  ),
+};
+
+const uint16_t keymaps_size = sizeof(keymaps);
+
+
+void matrix_init_user(void) {
+
+}
+
+void matrix_scan_user(void) {
+}
+
+// Code to run after initializing the keyboard
+void keyboard_post_init_user(void) {
+    // Here are two common functions that you can use. For more LED functions, refer to the file "qmk_ap2_led.h"
+
+    // annepro2-shine disables LEDs by default. Uncomment this function to enable them at startup.
+    annepro2LedEnable();
+
+    // Additionally, it also chooses the first LED profile by default. Refer to the "profiles" array in main.c in
+    // annepro2-shine to see the order. Replace "i" with the index of your preferred profile. (i.e the RED profile is index 0)
+    annepro2LedSetProfile(1);
+}
+
+layer_state_t layer_state_set_user(layer_state_t layer) {
+  switch(get_highest_layer(layer)) {
+    case _VIM_LAYER:
+      // Set the leds to green
+      annepro2LedSetForegroundColor(0x00, 0xFF, 0x00);
+      break;
+    case _FN_LAYER:
+      // Set the leds to blue
+      annepro2LedSetForegroundColor(0x00, 0x00, 0xFF);
+      break;
+    default:
+      // Reset back to the current profile
+      annepro2LedResetForegroundColor();
+      break;
+  }
+  return layer;
+}

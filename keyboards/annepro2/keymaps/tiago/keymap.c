d037501c4b3c7ff9770e2bb0accc0bf7e001c7e8
Tiago Donato <tgdnt@pm.me>
2022-03-29 22:57:43 -0500

    removed colemak and added caps tap bspc

--- keyboards/annepro2/keymaps/tiago/keymap.c
+++ keyboards/annepro2/keymaps/tiago/keymap.c
@@ -5,7 +5,6 @@
 enum anne_pro_layers {
   _BASE_LAYER,
   _DVORAK_LAYER,
-  _COLEMAK_LAYER,
   _VIM_LAYER,
   _FN_LAYER,
 };
@@ -43,7 +42,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE_LAYER] = KEYMAP( /* Base */
     LT(_FN_LAYER, KC_ESC), KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL,   KC_BSPC,
     LT(_VIM_LAYER, KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC,  KC_GRAVE,
-    KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,         KC_ENT,
+    LT(KC_LCTL, KC_BSPC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,         KC_ENT,
     KC_LSFT,KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
     KC_RALT,KC_LGUI, KC_LALT,                KC_SPC,               KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT
   ),
@@ -63,28 +62,8 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_DVORAK_LAYER] = KEYMAP( /* Base */
     LT(_FN_LAYER, KC_ESC), KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_LBRC, KC_RBRC,   KC_BSPC,
     LT(_VIM_LAYER, KC_TAB), KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y, KC_F, KC_G, KC_C, KC_R, KC_L, KC_SLSH, KC_EQL,  KC_GRAVE,
-    KC_LCTL, KC_A, KC_O, KC_E, KC_U, KC_I, KC_D, KC_H, KC_T, KC_N, KC_S, KC_MINS,         KC_ENT,
+    LT(KC_LCTL, KC_BSPC), KC_A, KC_O, KC_E, KC_U, KC_I, KC_D, KC_H, KC_T, KC_N, KC_S, KC_MINS,         KC_ENT,
     KC_LSFT,KC_SCLN, KC_Q, KC_J, KC_K, KC_X, KC_B, KC_M, KC_W, KC_V, KC_Z, KC_RSFT,
-    KC_RALT,KC_LGUI, KC_LALT,                KC_SPC,               KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT
-  ),
-  /* Layer _COLEMAK_LAYER
-  * ,-----------------------------------------------------------------------------------------.
-  * | Esc |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |    Bksp   |
-  * |-----------------------------------------------------------------------------------------+
-  * | Tab    |  q  |  w  |  f  |  p  |  g  |  j  |  l  |  u  |  y  |  ;  |  [  |  ]  |   ~    |
-  * |-----------------------------------------------------------------------------------------+
-  * | Ctrl     |  a  |  r  |  s  |  t  |  d  |  h  |  n  |  e  |  i  |  o  |  '  |    Enter   |
-  * |-----------------------------------------------------------------------------------------+
-  * | Shift      |  z  |  x  |  c  |  v  |  b  |  k  |  m  |  ,  |  .  |  /  |    Shift       |
-  * |-----------------------------------------------------------------------------------------+
-  * | AltGr  |  GUI   | Alt   |               space             | Left  | Down |  Up  | Right  |
-  * \-----------------------------------------------------------------------------------------/
-  */
-  [_COLEMAK_LAYER] = KEYMAP( /* Base */
-    LT(_FN_LAYER, KC_ESC), KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL,   KC_BSPC,
-    LT(_VIM_LAYER, KC_TAB), KC_Q, KC_W, KC_F, KC_P, KC_G, KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_LBRC, KC_RBRC,  KC_GRAVE,
-    KC_LCTL, KC_A, KC_R, KC_S, KC_T, KC_D, KC_H, KC_N, KC_E, KC_I, KC_O, KC_QUOT,         KC_ENT,
-    KC_LSFT,KC_Z, KC_X, KC_C, KC_V, KC_B, KC_K, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
     KC_RALT,KC_LGUI, KC_LALT,                KC_SPC,               KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT
   ),
   /* Layer _VIM_LAYER

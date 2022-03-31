2a0ffd2d0f67e2836339b3acc02cf3d296ffdf35
Tiago Donato <tgdnt@pm.me>
2022-03-30 22:44:58 -0500

    right shift tap enter
    * also rename base layer to qwerty layer

--- keyboards/annepro2/keymaps/tiago/keymap.c
+++ keyboards/annepro2/keymaps/tiago/keymap.c
@@ -3,7 +3,7 @@
 #include "qmk_ap2_led.h"
 
 enum anne_pro_layers {
-  _BASE_LAYER,
+  _QWERTY_LAYER,
   _DVORAK_LAYER,
   _VIM_LAYER,
   _FN_LAYER,
@@ -14,7 +14,7 @@ enum anne_pro_layers {
 
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /*
-  * Layer _BASE_LAYER
+  * Layer _QWERTY_LAYER
   * ,-----------------------------------------------------------------------------------------.
   * | ~ |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |    Bksp   |
   * |-----------------------------------------------------------------------------------------+
@@ -26,7 +26,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * |-----------------------------------------------------------------------------------------+
   * | AltGr  |  GUI   | Alt   |               space             | Left  | Down |  Up  | Right  |
   * \-----------------------------------------------------------------------------------------/
-  * Layer HOLD in _BASE_LAYER
+  * Layer HOLD in _QWERTY_LAYER
   * ,-----------------------------------------------------------------------------------------.
   * | FN  |     |     |     |     |     |     |     |     |     |     |     |     |           |
   * |-----------------------------------------------------------------------------------------+
@@ -39,11 +39,11 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * |       |       |       |                                 |       |       |       |       |
   * \-----------------------------------------------------------------------------------------/
   */
-  [_BASE_LAYER] = KEYMAP( /* Base */
+  [_QWERTY_LAYER] = KEYMAP( /* Base */
     LT(_FN_LAYER, KC_ESC), KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL,   KC_BSPC,
     LT(_VIM_LAYER, KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC,  KC_GRAVE,
     CTL_T(KC_BSPC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,         KC_ENT,
-    KC_LSFT,KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+    KC_LSFT,KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_SFTENT,
     KC_RALT,KC_LGUI, KC_LALT,                KC_SPC,               KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT
   ),
   /* Layer _DVORAK_LAYER
@@ -63,7 +63,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LT(_FN_LAYER, KC_ESC), KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_LBRC, KC_RBRC,   KC_BSPC,
     LT(_VIM_LAYER, KC_TAB), KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y, KC_F, KC_G, KC_C, KC_R, KC_L, KC_SLSH, KC_EQL,  KC_GRAVE,
     CTL_T(KC_BSPC), KC_A, KC_O, KC_E, KC_U, KC_I, KC_D, KC_H, KC_T, KC_N, KC_S, KC_MINS,         KC_ENT,
-    KC_LSFT,KC_SCLN, KC_Q, KC_J, KC_K, KC_X, KC_B, KC_M, KC_W, KC_V, KC_Z, KC_RSFT,
+    KC_LSFT,KC_SCLN, KC_Q, KC_J, KC_K, KC_X, KC_B, KC_M, KC_W, KC_V, KC_Z, KC_SFTENT,
     KC_RALT,KC_LGUI, KC_LALT,                KC_SPC,               KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT
   ),
   /* Layer _VIM_LAYER

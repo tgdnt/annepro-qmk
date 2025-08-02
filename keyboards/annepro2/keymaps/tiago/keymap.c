c6fff7c3943cbe5763b601a4d63441c58414cc2d
Tiago Donato <mail@tiagodonato.net>
2025-08-02 14:32:08 -0500

    update my layout

--- keyboards/annepro2/keymaps/tiago/keymap.c
+++ keyboards/annepro2/keymaps/tiago/keymap.c
@@ -3,8 +3,7 @@
 #include "qmk_ap2_led.h"
 
 enum anne_pro_layers {
-  _QWERTY_LAYER,
-  _DVORAK_LAYER,
+  _DVORAK_LAYER
   _VIM_LAYER,
   _FN_LAYER,
 };
@@ -14,19 +13,18 @@ enum anne_pro_layers {
 
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /*
-  * Layer _QWERTY_LAYER
   * ,-----------------------------------------------------------------------------------------.
-  * | ~ |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |    Bksp   |
+  * | Esc |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  [  |  ]  |    Bksp   |
   * |-----------------------------------------------------------------------------------------+
-  * | Tab    |  q  |  w  |  e  |  r  |  t  |  y  |  u  |  i  |  o  |  p  |  [  |  ]  |   \    |
+  * | Tab    |  '  |  ,  |  .  |  p  |  y  |  f  |  g  |  c  |  r  |  l  |  /  |  =  |   ~    |
   * |-----------------------------------------------------------------------------------------+
-  * | Ctrl     |  a  |  s  |  d  |  f  |  g  |  h  |  j  |  k  |  l  |  ;  |  '  |    Enter   |
+  * | Ctrl     |  a  |  o  |  e  |  u  |  i  |  d  |  h  |  t  |  n  |  s  |  -  |    Enter   |
   * |-----------------------------------------------------------------------------------------+
-  * | Shift      |  z  |  x  |  c  |  v  |  b  |  n  |  m  |  ,  |  .  |  /  |    Shift       |
+  * | Shift      |  ;  |  q  |  j  |  k  |  x  |  b  |  m  |  w  |  v  |  z  |    Shift       |
   * |-----------------------------------------------------------------------------------------+
   * | AltGr  |  GUI   | Alt   |               space             | Left  | Down |  Up  | Right  |
   * \-----------------------------------------------------------------------------------------/
-  * Layer HOLD in _QWERTY_LAYER
+  * HOLD in _DVORAK_LAYER
   * ,-----------------------------------------------------------------------------------------.
   * | FN  |     |     |     |     |     |     |     |     |     |     |     |     |           |
   * |-----------------------------------------------------------------------------------------+
@@ -39,30 +37,10 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * |       |       |       |                                 |       |       |       |       |
   * \-----------------------------------------------------------------------------------------/
   */
-  [_QWERTY_LAYER] = KEYMAP( /* Base */
-    LT(_FN_LAYER, KC_ESC), KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL,   KC_BSPC,
-    LT(_VIM_LAYER, KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC,  KC_GRAVE,
-    CTL_T(KC_BSPC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,         KC_ENT,
-    KC_LSFT,KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_SFTENT,
-    KC_RALT,KC_LGUI, KC_LALT,                KC_SPC,               KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT
-  ),
-  /* Layer _DVORAK_LAYER
-  * ,-----------------------------------------------------------------------------------------.
-  * | Esc |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  [  |  ]  |    Bksp   |
-  * |-----------------------------------------------------------------------------------------+
-  * | Tab    |  '  |  ,  |  .  |  p  |  y  |  f  |  g  |  c  |  r  |  l  |  /  |  =  |   ~    |
-  * |-----------------------------------------------------------------------------------------+
-  * | Ctrl     |  a  |  o  |  e  |  u  |  i  |  d  |  h  |  t  |  n  |  s  |  -  |    Enter   |
-  * |-----------------------------------------------------------------------------------------+
-  * | Shift      |  ;  |  q  |  j  |  k  |  x  |  b  |  m  |  w  |  v  |  z  |    Shift       |
-  * |-----------------------------------------------------------------------------------------+
-  * | AltGr  |  GUI   | Alt   |               space             | Left  | Down |  Up  | Right  |
-  * \-----------------------------------------------------------------------------------------/
-  */
   [_DVORAK_LAYER] = KEYMAP( /* Base */
     LT(_FN_LAYER, KC_ESC), KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_LBRC, KC_RBRC,   KC_BSPC,
     LT(_VIM_LAYER, KC_TAB), KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y, KC_F, KC_G, KC_C, KC_R, KC_L, KC_SLSH, KC_EQL,  KC_GRAVE,
-    CTL_T(KC_BSPC), KC_A, KC_O, KC_E, KC_U, KC_I, KC_D, KC_H, KC_T, KC_N, KC_S, KC_MINS,         KC_ENT,
+    KC_LCTL, KC_A, KC_O, KC_E, KC_U, KC_I, KC_D, KC_H, KC_T, KC_N, KC_S, KC_MINS,         KC_ENT,
     KC_LSFT,KC_SCLN, KC_Q, KC_J, KC_K, KC_X, KC_B, KC_M, KC_W, KC_V, KC_Z, KC_SFTENT,
     KC_RALT,KC_LGUI, KC_LALT,                KC_SPC,               KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT
   ),
@@ -82,29 +60,29 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_VIM_LAYER] = KEYMAP( /* Base */
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_END, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_TRNS, KC_TRNS,   KC_DELETE,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_END,  KC_BSLS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_PGDN, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_TRNS, KC_TRNS,         KC_TRNS,
-    KC_TRNS,KC_TRNS, KC_DELETE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS,KC_TRNS, KC_TRNS,      KC_TRNS,             KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_TRNS, KC_TRNS, KC_PGDN, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_TRNS,         KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_DELETE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,             KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS
   ),
   /* Layer _FN_LAYER
   * ,-----------------------------------------------------------------------------------------.
   * |     | F1  | F2  | F3  | F4  | F5  | F6  | F7  | F8  | F9  | F10 | F11 | F12 |           |
   * |-----------------------------------------------------------------------------------------+
-  * |   USB  | BT1 | BT2 | BT3 | BT4 |     |     | Wheel ^ |     |    |    |    |    | LED_On |
+  * |   USB  | BT1 | BT2 | BT3 | BT4 |     |     |         |     |    |    |    |    | LED_On |
   * |-----------------------------------------------------------------------------------------+
-  * | BT_UN   |    |    | Wheel V |    |    | <-  | \/  |  ^  |  -> |     |     | LED_Off     |
+  * | BT_UN   |    |    |         |    |    |     |     |     |     |     |     | LED_Off     |
   * |-----------------------------------------------------------------------------------------+
-  * |            |     |     |     |     | M1  | M2  |     |QWERTY|DVORAK|COLEMAK|            |
+  * |            |     |     |     |     |     |     |     |      |      |       |            |
   * |-----------------------------------------------------------------------------------------+
   * |       |       |       |                                 |       |       |       |       |
   * \-----------------------------------------------------------------------------------------/
   */
   [_FN_LAYER] = KEYMAP( /* Base */
-    KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS,
-    KC_AP2_USB, KC_AP2_BT1, KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, KC_TRNS, KC_TRNS, KC_MS_WH_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_WH_LEFT, KC_MS_WH_RIGHT, KC_AP_LED_ON,
-    KC_AP2_BT_UNPAIR, KC_TRNS, KC_TRNS, KC_MS_WH_DOWN, KC_TRNS, KC_TRNS, KC_MS_LEFT, KC_MS_DOWN, KC_MS_UP, KC_MS_RIGHT, KC_TRNS, KC_TRNS, KC_AP_LED_OFF,
-    KC_TRNS,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_BTN1, KC_MS_BTN2, KC_TRNS, TO(_QWERTY_LAYER), TO(_DVORAK_LAYER), KC_TRNS, KC_TRNS,
-    KC_TRNS,KC_TRNS, KC_TRNS,      KC_TRNS,             KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS
+    KC_TRNS,          KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS,
+    KC_AP2_USB,       KC_AP2_BT1, KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_AP_LED_ON,
+    KC_AP2_BT_UNPAIR, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_AP_LED_OFF,
+    KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS,          KC_TRNS, KC_TRNS,      KC_TRNS,             KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS
   ),
 };
 
